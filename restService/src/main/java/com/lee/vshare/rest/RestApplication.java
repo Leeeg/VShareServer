d4ad2032f99cac0fb84fd41a623da9d960e9cf98
Leeeg <873793992@qq.com>
2019-05-21 00:19:47 +0800

    完善dubbo配置

--- restService/src/main/java/com/lee/vshare/rest/RestApplication.java
+++ restService/src/main/java/com/lee/vshare/rest/RestApplication.java
@@ -3,10 +3,7 @@ package com.lee.vshare.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @EnableSwagger2
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class},
